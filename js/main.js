d88f36e5a94898129c12d9d8017ec76b1346dff9
naoyasugiyama <lynx07talisman@gmial.com>
2019-11-26 15:30:31 +0900

    リストの表示

---
+++ js/main.js
@@ -0,0 +1,14 @@
+(function(){
+    'use strict';
+
+    var rm = new Vue({
+        el: '#app',
+        data: {
+            todos:[
+                'task1',
+                'task2',
+                'task3'
+            ]
+        }
+    });
+})();
