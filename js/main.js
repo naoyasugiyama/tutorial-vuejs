a04d707f7f0ecbca380f3c303508cb92e381c1b6
naoyasugiyama <lynx07talisman@gmial.com>
2019-11-26 19:53:17 +0900

    input,img

--- js/main.js
+++ js/main.js
@@ -8,7 +8,27 @@
                 'task1',
                 'task2',
                 'task3'
-            ]
+            ],
+            title: 'Hello World!',
+            input_title: 'null',
+            link: 'http://google.com',
+            // クロスサイトスクリプティング注意
+            finishedLink: '<a href="http://google.com">Google</a>',
+            img_path: '',
+            name: 'NSUGIYAMA'
+        },
+        methods:{
+            // input key
+            changeTitle: function(){
+                this.input_title = event.target.value;
+            },
+            seyHelloy: function(){
+                this.tilte='Hello'
+                return this.title;
+            },
+            SetName: function(){
+                return this.name;
+            }
         }
     });
 })();
